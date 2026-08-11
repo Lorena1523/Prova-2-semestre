a8efa6ef01b5a57ea5e3d5d646d7b42dd5630e03
NicolasCoutinho669 <nicolasrvc2@gmail.com>
2026-08-11 07:52:39 -0300

    Primeira def

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-s
+import csv
+import os
+def cadastrar_livro():
+    titulo = input("Digite o título do livro: ")
+    autor = input("Digite o autor do livro: ")
+    ano = input("Digite o ano de publicação do livro: ")
+    with open('livros.csv', mode='a', newline='') as arquivo:
+        escritor_csv = csv.writer(arquivo)
+        escritor_csv.writerow([titulo, autor, ano])
+
+print("Livro cadastrado com sucesso!")
+print("======Biblioteca======")
+print("1- Cadastrar livro")
+print("2- Listar livros")
+print("3- Pesquisar livro")
+print("4- Registrar empréstimo")
+print("5- Registrar devolução")
+print("6- Ordenar livros")
+print("7- Sair")
+opcao = int(input("Digite a opção desejada: "))
+
+if opcao == 1:
+    cadastrar_livro()
